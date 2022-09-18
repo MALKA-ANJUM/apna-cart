946b91aea9c17cf775ccbec2103b74b86ccbb88f
Malka <malkaanjum1999@gmail.com>
2022-09-18 23:26:08 +0530

    Day-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
 
 function App() {
+  const product = [
+    {
+      price: 99999,
+      product: "iphone",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      product: "samsung",
+      quantity: 0,
+    },
+    {
+      price: 999,
+      product: "jio next",
+      quantity: 0,
+    }
+  ]
   return (
     <>
-      
+      <Navbar />
+      <ProductList />
+      <Footer />
     </>
   );
 }
